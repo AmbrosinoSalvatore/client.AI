295ba3e88255357832f7d27fd914674fbbe9198b
AmbrosinoSalvatore <ambrosino.salvatore@iisgalvanimi.edu.it>
2024-06-06 11:52:49 +0000

    gg

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AnimalsService } from './animals.service';
+import { Animal, VettAnimal } from './animals.service';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'client';
+  data = new Array<Animal>();
+
+  //Per la reactive form creo due proprietà che conterranno i valori delle caselle di testo
+  form = new FormGroup({
+    "name": new FormControl(),
+    "type": new FormControl(),
+  });
+
+  onSubmit() {
+    console.log("reactive form submitted");
+    console.log(this.form.controls['name'].value);
+    console.log(this.form.controls['type'].value);
+    let a : Animal = {
+      "id": '0',
+      "name":this.form.controls['name'].value,
+      "type": this.form.controls['type'].value
+    };
+
+    //Quando ricevo una risposta dal server aggiorno l'id dell'animale e lo invio al vettore data
+    this.animalService.sendNewAnimal(a).subscribe(
+      (data)=>{
+        console.log(data);
+        a.id = data['id'];
+        this.data.push(a);
+      }
+    )
+  }
+  
+  //Mi faccio iniettare l'animal servce
+  constructor(private animalService : AnimalsService)
+  {
+    //Mi sottoscrivo al servizio
+    console.log("hello")
+    this.animalService.getAnimals().subscribe(
+      (data: VettAnimal)=>{this.data = data['animals']}
+    )
+  }
+
 }
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  baseUrlServer: 'https://5000-ambrosinosalva-clientai-wi7391yee3b.ws-eu114.gitpod.io/'
 };
 
 /*
